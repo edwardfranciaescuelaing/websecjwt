33e5f32bb27a860de24e339678aa299354e8c594
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-11-16 09:03:40 -0500

    actualizado

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_zOpvMqjiQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '106qd56u5jk5v60b2ame4illls', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
